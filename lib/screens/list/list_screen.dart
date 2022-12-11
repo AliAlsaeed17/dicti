29d5b54f320f62edf6cf192c854e25b38a3869ef
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-12-11 10:53:05 +0300

    Update words list

--- lib/screens/list/list_screen.dart
+++ lib/screens/list/list_screen.dart
@@ -1,10 +1,24 @@
+import 'package:dicti/model/word_response.dart';
 import 'package:flutter/material.dart';
 
 class ListScreen extends StatelessWidget {
-  const ListScreen({Key? key}) : super(key: key);
+  List<WordResponse> words;
+
+  ListScreen(this.words);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(words[index].word!),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(color: Colors.grey),
+        itemCount: words.length,
+      ),
+    );
   }
 }
