036869976f008dfcf4d24f7863db1134d6fad477
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-12-11 11:21:37 +0300

    Update details screen

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,10 +1,64 @@
+import 'package:dicti/model/word_response.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({Key? key}) : super(key: key);
+  final WordResponse wordResponse;
+
+  DetailsScreen(this.wordResponse);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Container(
+        padding: EdgeInsets.all(32),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text(
+              "${wordResponse.word}",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+              ),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  final meanings = wordResponse.meanings![index];
+                  final defenitions = meanings.definitions;
+                  return Column(
+                    children: [
+                      Text(
+                        meanings.partOfSpeech!,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(height: 8),
+                      ListView.separated(
+                        itemBuilder: (context, index) => Column(
+                          children: [
+                            Text("Definition: " +
+                                defenitions![index].definition!),
+                            SizedBox(height: 8),
+                            Text("Example: " + defenitions![index].example!),
+                          ],
+                        ),
+                        separatorBuilder: (context, index) =>
+                            SizedBox(height: 8),
+                        itemCount: defenitions!.length,
+                        shrinkWrap: true,
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(height: 32),
+                itemCount: wordResponse.meanings!.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
